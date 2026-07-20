50fad56ca841c30f4cdf3de2a4a0817b88432f13
astritcetzal <astrit.cetzal@tecdesoftware.edu.mx>
2026-07-20 08:14:29 -0600

    chore: initial SpringBoot project setup

---
+++ src/main/java/mx/edu/tecdesoftware/Astrit_restaurant_backend/AstritRestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.Astrit_restaurant_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstritRestaurantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstritRestaurantBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/tecdesoftware/Astrit_restaurant_backend/AstritRestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.tecdesoftware.Astrit_restaurant_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstritRestaurantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
